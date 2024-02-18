ba57e4cb9d4892cb756c90758d600636bb20f49a
amen <amenferjani23@gmail.com>
2024-02-18 22:53:45 +0100

    complete user api endpoints

--- server/app.js
+++ server/app.js
@@ -8,16 +8,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const userRoutes = require('./routes/user');
-
+const messageRoutes = require('./routes/messages');
+const conversationRoutes = require('./routes/conversation');
 
 app.use('/api/user', userRoutes);
+// app.use('/api/message', messageRoutes);
+// app.use('/api/conversation', conversationRoutes);
 
+const PORT = process.env.PORT || 8800;
 
-    const PORT = process.env.PORT || 8800;
-
-    app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-
 });
 
 module.exports = app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,24 +1,31 @@
     const express = require('express');
     const router = express.Router();
+    const multer = require('multer');
     const controller = require('../controller/controller');
 
-//!! Endpoint for getting user by ID
+    const upload = multer({ 
+        storage:  multer.memoryStorage(),
+    });
+
+    //? Endpoint for getting user by ID
     router.get('/getUserById/:userId',
         controller.getUserById);
 
-    // //!Endpoint for getting user by email and password
+    //? Endpoint for getting user by email and password
     router.post('/get-user-login-info',
         controller.getUserByEmailAndPassword);
-    // //!! Endpoint for creating a new user
+
+    //?  Endpoint for creating a new user
     router.post('/',
         controller.createUser);
 
-    // //!! Endpoint for updating user password
-    // router.put('/:id/update-password',
-    //     controller.updateUserPassword);
+    //? Endpoint for updating user password
+    router.put('/:id/update-password',
+        controller.updateUserPassword);
 
-    // //!! Endpoint for updating user image
-    // router.put('/:userId/update-image/:imageId',
-    //     controller.updateUserImage);
+    //? Endpoint for updating user image
+    router.put('/update-user-image',
+        upload.single('profilePicture'),
+        controller.updateUserImage);
 
     module.exports = router;
