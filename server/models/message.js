aa3fd734ab84889bf5dae5c2d4ef0c42847e8ffd
amen <amenferjani23@gmail.com>
2024-02-19 14:56:28 +0100

    .

--- server/models/message.js
+++ server/models/message.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    conversation : {type: mongoose.Schema.Types.ObjectId, ref: 'conversation'},
+    conversation: { type: mongoose.Schema.Types.ObjectId, ref: 'conversation' },
     content: String,
     timestamp: { type: Date, default: Date.now },
     seen: Boolean,
-})
+});
 
 const message = mongoose.model('message', messageSchema);
     
