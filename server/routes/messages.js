1718284f3f4babb77e23c0124422e8d28c46a83b
amen <amenferjani23@gmail.com>
2024-02-21 20:00:33 +0100

    complete message api endpoints

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -2,19 +2,19 @@
     const router = express.Router();
     const controller = require('../controller/controller');
 
-//! Endpoint for getting msg by ID
+//?Endpoint for getting msg by ID
     router.get('/:id',
         messageController.getMessageById);
         
-//!  Endpoint for getting messages by Conversation ID
+//? Endpoint for getting messages by Conversation ID
     router.get('/conversation/:id/',
         controller.getMessagesByConversationId);
 
-//!! Endpoint for adding a new msg
+//? Endpoint for adding a new msg
     router.post('/user/:userId/conversation/:conversationId',
         controller.addMessage);
 
-//!! Endpoint for deleting a msg
+//? Endpoint for deleting a msg
     router.delete('/:id',
         messageController.deleteMessage);
 
